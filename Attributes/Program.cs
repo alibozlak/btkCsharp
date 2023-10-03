6a4bc1c059b3256d8adc2ee204fef38ecd925649
alibozlak <ali-bozlak@outlook.com>
2023-10-03 15:47:39 +0300

    Added Attributes  section, fist lesson

---
+++ Attributes/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Ali"
+            };
+
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            customerDal.AddNew(customer);
+        }
+    }
+
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class RequiredPropertyAttribute : Attribute
+    {
+        
+    }
+
+    class ToTableAttribute : Attribute
+    {
+        private string tableName;
+        public ToTableAttribute(string tableName)
+        {
+            this.tableName = tableName;
+        }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew method")]  // ---> in Java @Deprecated annotation
+        public  void Add(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName} added!");
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine($"{customer.FirstName} added with new add method!");
+        }
+    }
+}
