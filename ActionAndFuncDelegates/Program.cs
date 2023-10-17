7453a632df068ca5fea7bf04f89b19c0a8840ecc
alibozlak <ali-bozlak@outlook.com>
2023-10-17 21:18:28 +0300

    Added Delegates, Action and Func

---
+++ ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace ActionAndFuncDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 15, 16, 17, 18 };
+
+            Console.WriteLine("------ Action --------");
+
+            Action action1 = () =>
+            {
+                Console.WriteLine(intArray[12]);
+            };
+
+            ExceptionHandle(action1);
+
+            Console.WriteLine("---------");
+
+            ExceptionHandle(() =>
+            {
+                intArray[0] = 20;
+                Console.WriteLine(intArray[0]);
+            });
+
+            Console.WriteLine("------ Func --------");
+
+            Func<int, bool> func1 = (int integer) => integer > 17;
+
+            List<int> greaterThan17ints = FilterArray(func1, intArray);
+            foreach (var integer in greaterThan17ints)
+            {
+                Console.Write(integer + " ");
+            }
+            Console.WriteLine("\n----");
+
+            List<int> lessThan17ints = FilterArray((int integer) => integer < 17, intArray);
+            foreach (var integer in lessThan17ints)
+            {
+                Console.Write(integer + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void ExceptionHandle(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine("Dizinin olmayan bir indexini okumaya çalışitınız!!! Message : ");
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static List<int> FilterArray(Func<int,bool> func, int[] integers) 
+        {
+            List<int> resultList = new List<int>();
+            foreach (int integer in integers)
+            {
+                if (func.Invoke(integer))
+                {
+                    resultList.Add(integer);
+                }
+            }
+            return resultList;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        public delegate void MyDelegate();     // <-- Delegate of No parameters and void return type methods
+        public delegate void MyDelegate2(string text);
+
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+            myDelegate();
+            Console.WriteLine("Minus SendMessage method from myDelegate :");
+            myDelegate -= customerManager.SendMessage;
+            myDelegate();
+
+            Console.WriteLine("---------");
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+            myDelegate2("Bla bla bla");
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!");
+        }
+
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine("Hello! {0}", message);
+        }
+
+        public void ShowAlert2(string message)
+        {
+            Console.WriteLine("Be careful! {0}", message);
+        }
+    }
+}
