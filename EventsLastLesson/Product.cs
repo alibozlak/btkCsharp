3eea05af2d98b6e09d797d35550194ea14f83afe
alibozlak <ali-bozlak@outlook.com>
2023-10-26 20:08:39 +0300

    Added Events lesson

---
+++ EventsLastLesson/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsLastLesson
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stockAmount;
+        public Product(string productName, int stockAmount)
+        {
+            _stockAmount = stockAmount;
+            ProductName = productName;
+        }
+
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int StockAmount
+        {
+            get { return _stockAmount; }
+            set
+            {
+                _stockAmount = value;
+                if (value <= 20 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            if (amount <= _stockAmount)
+            {
+                StockAmount -= amount;
+                Console.WriteLine($"{ProductName} stok miktarı : {StockAmount}");
+            } else
+            {
+                Console.WriteLine($"Stokta {ProductName} ürününden {amount} adet maalesef ki yok!! Satış yapılamadı!!");
+            }
+        }
+    }
+}
---
+++ EventsLastLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EventsLastLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hardDisk = new Product("Hard Disk", 42);
+            Product mobilePhone = new Product("iPhone XI", 42);
+
+            mobilePhone.StockControlEvent += MobilePhone_StockControlEvent;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("---- {0}. Satış ----",i+1);
+                hardDisk.Sell(10);
+                mobilePhone.Sell(10);
+                Console.ReadKey();
+            }
+        }
+
+        private static void MobilePhone_StockControlEvent()
+        {
+            Console.WriteLine("STOKTA iPhone XI AZALDI, LÜTFEN SİPARİŞ VER!!");
+        }
+    }
+}
